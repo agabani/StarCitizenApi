b49d3203f9051975723bc10f9c9ffee6d0c228fd
Ahmed Agabani <agabani@live.com>
2017-03-18 21:56:18 +0000

    Null cache

---
+++ StarCitizenApi/ArkStarmap/Cache/NullCache.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace StarCitizenApi.ArkStarmap.Cache
+{
+    public class NullCache : ICache
+    {
+        public Task<string> Get(string endpoint, string body)
+        {
+            return Task.FromResult<string>(null);
+        }
+
+        public Task Put(string endpoint, string body, string content)
+        {
+            return Task.FromResult<object>(null);
+        }
+    }
+}
