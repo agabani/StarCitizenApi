e8cd3136a7a58f22834c4d5e76233043b5156a74
Ahmed Agabani <agabani@live.com>
2017-03-18 00:42:14 +0000

    nuspec

--- StarCitizenApi/Properties/AssemblyInfo.cs
+++ StarCitizenApi/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StarCitizenApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Star Citizen API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("agabani")]
 [assembly: AssemblyProduct("StarCitizenApi")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
