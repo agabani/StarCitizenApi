b2cffebb77124f381340faa060024f4205f8855b
Ahmed Agabani <agabani@live.com>
2017-03-31 21:35:45 +0100

    Azure Blob Cache

---
+++ StarCitizenApi.WindowsAzure/Cache/BlobCache.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using StarCitizenApi.ArkStarmap.Cache;
+
+namespace StarCitizenApi.WindowsAzure.Cache
+{
+    public class BlobCache : ICache
+    {
+        private readonly CloudBlobContainer _container;
+
+        public BlobCache(CacheOptions options)
+        {
+            var storageAccount = CloudStorageAccount.Parse(options.ConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            _container = blobClient.GetContainerReference(options.Container);
+            _container.CreateIfNotExistsAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task<string> Get(string endpoint, string body)
+        {
+            var blockBlobReference = _container
+                .GetBlockBlobReference(FileName(endpoint, body));
+
+            return await blockBlobReference.ExistsAsync()
+                ? await blockBlobReference.DownloadTextAsync(Encoding.UTF8, null, null, null)
+                : null;
+        }
+
+        public async Task Put(string endpoint, string body, string content)
+        {
+            await _container
+                .GetBlockBlobReference(FileName(endpoint, body))
+                .UploadTextAsync(content, Encoding.UTF8, null, null, null);
+        }
+
+        private static string FileName(string endpoint, string body = "")
+        {
+            byte[] computeHash;
+
+            using (var md5 = MD5.Create())
+            {
+                computeHash = md5.ComputeHash(Encoding.UTF8.GetBytes($"{endpoint}{body}"));
+            }
+
+            var builder = new StringBuilder();
+
+            foreach (var @byte in computeHash)
+                builder.Append(@byte.ToString("x2"));
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ StarCitizenApi.WindowsAzure/Cache/CacheOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace StarCitizenApi.WindowsAzure.Cache
+{
+    public class CacheOptions
+    {
+        public string ConnectionString { get; set; }
+        public string Container { get; set; }
+    }
+}
